540c0ed0cc2711b943bb1b494a33a76b4c260549
Herun Tarun <herun.s@gmail.com>
2026-04-30 14:56:13 -0400

    added decorator for skpping a test

--- lesson3/test_car.py
+++ lesson3/test_car.py
@@ -6,6 +6,10 @@ class CarTest(unittest.TestCase):
         car = Car()
         self.assertEqual(4, car.wheels)
 
+    @unittest.skip('skipping a bad test')
+    def test_bad_wheels(self):
+        car = Car()
+        self.assertEqual(3, car.wheels)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
